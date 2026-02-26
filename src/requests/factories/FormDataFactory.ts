fed447529c13d78d1c87895ac993828bdf374a34
alexander.hank <mail@hank-it.com>
2026-02-26 23:33:59 +0100

    fix: Loosen typeing in FormDataFactory.ts

--- src/requests/factories/FormDataFactory.ts
+++ src/requests/factories/FormDataFactory.ts
@@ -3,10 +3,13 @@ import { type BodyFactoryContract } from '../contracts/BodyFactoryContract'
 import { type BodyContract } from '../contracts/BodyContract'
 
 type FormDataPrimitive = string | number | boolean | null | Date | Blob
-type FormDataValue = FormDataPrimitive | FormDataValue[] | { [key: string]: FormDataValue }
+
+export type FormDataValue = FormDataPrimitive | FormDataValue[] | { [key: string]: FormDataValue }
 
 export class FormDataFactory<
-  RequestBodyInterface extends Record<string, FormDataValue | undefined>
+  RequestBodyInterface extends {
+    [K in keyof RequestBodyInterface]: FormDataValue | undefined
+  }
 > implements BodyFactoryContract<RequestBodyInterface> {
   public make(body: RequestBodyInterface): BodyContract {
     return new FormDataBody<RequestBodyInterface>(body)
